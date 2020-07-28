3091ce3f2da565681164d8db71dc5db314feecab
omerHamid <66900959+omerHamid@users.noreply.github.com>
2020-07-29 00:17:36 +0530

    Updates

---
+++ precache-manifest.6b347453b53e100767bcf768792724f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d60ff47cb5e91afcee135c85bad9df3",
+    "url": "/simpleToDoList/index.html"
+  },
+  {
+    "revision": "f4410bf61fd8cf9d2f1d",
+    "url": "/simpleToDoList/static/css/main.3bd0776f.chunk.css"
+  },
+  {
+    "revision": "5a81f1ba0d485276bd05",
+    "url": "/simpleToDoList/static/js/2.9b8e7151.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simpleToDoList/static/js/2.9b8e7151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4410bf61fd8cf9d2f1d",
+    "url": "/simpleToDoList/static/js/main.d7fb5e33.chunk.js"
+  },
+  {
+    "revision": "a6662e6f185db194dce8",
+    "url": "/simpleToDoList/static/js/runtime-main.aae52485.js"
+  }
+]);
